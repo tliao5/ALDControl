74dd90e2eb19991846ee839067b22f2fdf57cde4
tliao5 <tliao5@u.rochester.edu>
2025-05-27 23:34:55 -0400

    Add files via upload

---
+++ ald_controller.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+import logging
+import time
+import threading
+
+class ALDController:
+    def __init__(self):
+        self.stopthread = threading.Event()
+    ### 
+    # aldRun(file, loops, gvc) - executes an ALD Run
+    # file - recipe file read in to program
+    # loops - number of times to loop through the recipe
+    # vc - gas_valve_controller() object
+    ###
+    def create_run_thread(self,loops,vc):
+        self.aldRunThread = threading.Thread(target=self.aldRun, args=(loops, vc))
+        self.aldRunThread.start()
+
+    def aldRun(self, loops, vc):
+        data = pd.read_csv(self.file)
+        dataNP = data.to_numpy()
+        print(vc.tasks)
+        print(dataNP)
+        # log run starting and recipe order
+        print("Run Starting")
+        previndices = []
+        for i in range(loops): #This is the number of loops the user wants to iterate the current file (ie - number of ALD cycles)
+            if self.stopthread.isSet():
+                    break
+            print(f"Loop: {i+1}")
+            for j in range(0,len(dataNP),1):#For each row in the .csv file, we want to set the experimental parameters accordingly
+                print(f"Row: {j+1}")
+                if self.stopthread.isSet():
+                    break
+                row = dataNP[j][:-1].tolist()
+                indices = [index for index, val in enumerate(row) if val == 1]
+                print(f"Row: {row}, Indices: {indices}, PrevIndices: {previndices}")
+                indices = [index for index in indices if index not in previndices]
+                if indices:
+                    vc.pulse_valve(indices,dataNP[j][6])
+                else:
+                    print(f"Purging: {dataNP[j,6]}")
+                    time.sleep(dataNP[j][6])
+                previndices = indices
+                print()
+        print("Run Over")
+        vc.close_all() # make sure all valves are shut off at the end of a run
+
+    def close(self):
+        self.stopthread.set()
+        # Check if the thread exists and is initialized
+        if hasattr(self, 'aldRunThread') and self.aldRunThread is not None:
+            print("Waiting for ALD Run Thread to Close")
+            self.aldRunThread.join()
+        print("ALD Recipe Controller Closing")
---
+++ valve_controller.py
@@ -0,0 +1,68 @@
+import nidaqmx
+from nidaqmx.constants import LineGrouping
+import time
+import threading
+from config import VALVE_CHANNELS
+
+# creating a valve_controller object will setup all relevant channels
+# access said object in order to run methods on the valves connected to channels defined below
+
+# currently hard coded for the three valves of this ALD system
+class ValveController:
+    def __init__(self):
+        self.valvechannels = VALVE_CHANNELS
+        self.tasks = self.create_valve_tasks()
+        # log valve controller initialized
+        
+    def create_valve_tasks(self):
+        AV01 = nidaqmx.Task("AV01")
+        AV02 = nidaqmx.Task("AV02")
+        AV03 = nidaqmx.Task("AV03")
+        AV01.do_channels.add_do_chan(self.valvechannels["AV01"], line_grouping=LineGrouping.CHAN_PER_LINE)
+        AV02.do_channels.add_do_chan(self.valvechannels["AV02"], line_grouping=LineGrouping.CHAN_PER_LINE)
+        AV03.do_channels.add_do_chan(self.valvechannels["AV03"], line_grouping=LineGrouping.CHAN_PER_LINE)
+        return [AV01, AV02, AV03]
+
+        
+    def open_valve(self,task):
+        task.start()
+        task.write(True)
+        time.sleep(0.1)
+        #log valve opened
+        task.stop()
+        
+    def close_valve(self,task):
+        task.start()
+        task.write(False)
+        time.sleep(0.1)
+        #log valve closed
+        task.stop()
+
+    def pulse_valve(self,indices,pulse_length):
+        print(indices[::])
+        tasks = [self.tasks[i] for i in indices]
+        print(tasks)
+        for t in tasks[::]:
+            print(t.name)
+            t.start()
+            t.write(True)
+        time.sleep(pulse_length)
+        for t in tasks[::]:
+            print(t.name)
+            t.write(True)
+            t.stop()
+        #print(f"Task {task.name}: False")
+        #log valve pulsed
+
+    def close_all(self):
+        for task in self.tasks[::]:
+            task.start()
+            task.write(False)
+            time.sleep(0.1)
+            task.stop()
+        #log valves closed
+
+    def close(self):
+        self.close_all()
+        for task in self.tasks[::]: task.close()
+        print("Valve Controller Tasks Closing")
