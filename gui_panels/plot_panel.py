9579faa4c322b0195e5282dd2d6f8b4a09cab35a
tliao5 <tliao5@u.rochester.edu>
2025-07-08 16:28:23 -0400

    Show_temperatures

--- gui_panels/plot_panel.py
+++ gui_panels/plot_panel.py
@@ -8,17 +8,21 @@ import time
 import logging
 from config import *
 
+THERMOCOUPLE_COLORS = ["b","g","r","c","m","y","k"]
+
 class PlotPanel:
     def __init__(self, app):
         self.app = app
-        self.fig, self.ax, self.pressure, self.t_array, self.t_start, self.sensors = self.plot_initialize()
+        self.fig, self.ax, self.ax_right, self.pressure, self.t_array, self.t_start, self.sensors = self.plot_initialize()
         self.ymin = tk.StringVar(value=Y_MIN_DEFAULT)
         self.ymax = tk.StringVar(value=Y_MAX_DEFAULT)
+        self.show_temperatures = False
 
     def create_plot_panel(self, title):
         frame = tk.Frame(bg=TEXT_COLOR, highlightbackground=BORDER_COLOR, highlightthickness=1)
         frame.grid_rowconfigure(0, weight=1)
         frame.grid_columnconfigure(0, weight=1)
+        self.temp_plot_data = []
 
         ani = animation.FuncAnimation(self.fig, self.animate, interval=250, save_count=150, repeat=True)
         tempplot = FigureCanvasTkAgg(self.fig, frame)
@@ -35,6 +39,10 @@ class PlotPanel:
         tk.Entry(row, width=10, textvariable=self.ymin, font=FONT).pack(side=tk.LEFT, padx=5)
         tk.Label(row, text="y-max:", relief=BUTTON_STYLE, bg=BG_COLOR, font=FONT).pack(side=tk.LEFT, padx=5)
         tk.Entry(row, width=10, textvariable=self.ymax, font=FONT).pack(side=tk.LEFT, padx=5)
+        self.show_temperatures_button = tk.Button(
+            row, text='Show Temperatures OFF', fg=BUTTON_TEXT_COLOR, bg=OFF_COLOR, relief=BUTTON_STYLE,
+            command=self.toggle_show_temperatures)
+        self.show_temperatures_button.pack(padx=5)
 
         return frame
 
@@ -43,18 +51,30 @@ class PlotPanel:
         plt.rcParams["figure.autolayout"] = True
         plt.rcParams['font.size'] = 14
         fig, ax = plt.subplots()
+        ax_right = ax.twinx()
         pressure = self.app.log_controller.pressure_deque
         t_start = self.app.log_controller.t_start
         t_array = self.app.log_controller.t_array
         sensors = ["main reactor", "inlet lower", "inlet upper", "exhaust", "TMA", "Trap", "Gauges", "Pressure"]
-        return fig, ax, pressure, t_array, t_start, sensors
+        return fig, ax, ax_right, pressure, t_array, t_start, sensors
 
     def animate(self, i):
         try:
-            tempdata = self.app.log_controller.temperature_deque[-1]
-
             self.ax.clear()
+            self.ax_right.clear()
+
+            tempdata = self.app.log_controller.temperature_deque[-1]
+            
+
             self.ax.plot(self.t_array, self.pressure)
+            
+            # Plot the data using Matplotlib
+            if self.show_temperatures == True:
+                self.ax_right.set_visible(True) 
+                for i in range(6):
+                    self.ax_right.plot(self.t_array, [row[i] for row in self.app.log_controller.temperature_deque],THERMOCOUPLE_COLORS[i])
+            else:
+                self.ax_right.set_visible(False) 
 
             ymin = float(self.ymin.get())
             ymax = float(self.ymax.get())
@@ -65,10 +85,22 @@ class PlotPanel:
 
             for j, sensor in enumerate(self.sensors[:-1]):
                 y_position = ymin * (ymax / ymin) ** (0.4 + 0.07 * j)
-                self.ax.text(self.t_array[0] + 250, y_position, f"{sensor}, {str(tempdata[j])[:5]}")
+                if self.show_temperatures == True:
+                    color = THERMOCOUPLE_COLORS
+                else:
+                    color = ['k']*7
+                self.ax.text(self.t_array[0] + 250, y_position, f"{sensor}, {str(tempdata[j])[:5]}",color=color[j])
             self.fig.tight_layout()
         except Exception as e:
             logging.error("Error during animation: %s", e)
-    
+
+    def toggle_show_temperatures(self):
+        if self.show_temperatures_button['text'] == 'Show Temperatures ON':
+            self.show_temperatures = False
+            self.show_temperatures_button.config(text='Show Temperatures OFF', bg=OFF_COLOR)
+        elif self.show_temperatures_button['text'] == 'Show Temperatures OFF':
+            self.show_temperatures = True
+            self.show_temperatures_button.config(text='Show Temperatures ON', bg=ON_COLOR)
+
     def close(self):
         plt.close(self.fig)
