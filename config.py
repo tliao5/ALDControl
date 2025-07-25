7e640d6134ebd75a1a8e6362343682b0ae7a8d16
tliao5 <tliao5@u.rochester.edu>
2025-07-25 09:25:04 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -6,28 +6,32 @@ OFF_COLOR = "red4"
 BUTTON_STYLE = "raised"
 BUTTON_TEXT_COLOR = "grey85"
 BORDER_COLOR = "black"
-FONT = ("Helvetica", 16)
+FONT = ("Helvetica", 22)
 
 
 # Log output
-LOG_FILE = "PreasureTempJune.log"
+LOG_FILE = "HorizontalChamberJuly.log"
 MONITOR_LOG_FILE = "monitor.log"
 
 # Pressure plot default y min and max
-Y_MIN_DEFAULT = 0.550
-Y_MAX_DEFAULT = 0.650
+Y_MIN_DEFAULT = 0.575
+Y_MAX_DEFAULT = 0.610
 
 
 
 MAIN_POWER_CHANNEL = "CDAQ1Mod4/line11"
+
 VALVE_CHANNELS = { "AV01": "cDAQ1Mod4/line0", # TMA
                    "AV02": "CDAQ1Mod4/line1", # D20
-                   "AV03": "CDAQ1Mod4/line2"} # H20                      
+                   "AV03": "CDAQ1Mod4/line2"} # H20      
+                   
 HEATER_CHANNELS = { "h1channel": "CDAQ1Mod4/port0/line5", # heater 1
                     "h2channel": "CDAQ1Mod4/port0/line6", # heater 2
                     "h3channel": "CDAQ1Mod4/port0/line7", # heater 3
                     "h4channel": "CDAQ1Mod4/port0/line8"} # heater 4
-TEMP_CHANNELS = ["ai0", "ai1", "ai2", "ai3", "ai4", "ai5", "ai6"]
+TEMP_CHANNELS = ["ai0", "ai1", "ai2", "ai3", "ai4", "ai5", "ai6","ai7", "ai10", "ai13", "ai9", "ai14"]
+DUTY_CYCLE_LENGTH = 10 # seconds
+
 PRESSURE_CHANNEL = {"Pchannel":"cDAQ1Mod2/ai2"}
 
 MFC_PORT = "COM6"
