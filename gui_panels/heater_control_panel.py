c2e3ac7147e8353de31831b83244383e89539430
tliao5 <tliao5@u.rochester.edu>
2025-10-17 12:33:36 -0400

    Add files via upload

--- gui_panels/heater_control_panel.py
+++ gui_panels/heater_control_panel.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from config import *
 
 ## Heater Control Panel
 # Interfaces with temp_controller
@@ -10,49 +11,39 @@ import tkinter as tk
 class HeaterControlPanel:
     def __init__(self, app):
         self.app = app
-        self.HEATER_CHANNELS = app.HEATER_CHANNELS
-        self.BG_COLOR = app.BG_COLOR
-        self.BORDER_COLOR = app.BORDER_COLOR
-        self.FONT = app.FONT
-        self.TEXT_COLOR = app.TEXT_COLOR
-        self.BUTTON_TEXT_COLOR = app.BUTTON_TEXT_COLOR
-        self.BUTTON_STYLE = app.BUTTON_STYLE
-        self.LOG_FILE = app.LOG_FILE
-        self.ON_COLOR = app.ON_COLOR
-        self.OFF_COLOR = app.OFF_COLOR
         
     def create_heater_control_panel(self):
-        frame = tk.Frame(bg=self.BG_COLOR, highlightbackground=self.BORDER_COLOR, highlightthickness=1)
+        frame = tk.Frame(bg=BG_COLOR, highlightbackground=BORDER_COLOR, highlightthickness=1)
         
-        self.duty = [tk.StringVar() for i in range(len(self.HEATER_CHANNELS))] # duty entry field variable
-        self.max_temp = [tk.StringVar() for i in range(len(self.HEATER_CHANNELS))] # max temp entry field variable
+        self.duty = [tk.StringVar() for i in range(len(HEATER_CHANNELS))] # duty entry field variable
+        self.max_temp = [tk.StringVar() for i in range(len(HEATER_CHANNELS))] # max temp entry field variable
         
-        self.heater_buttons = [tk.Button() for i in range(len(self.HEATER_CHANNELS))]
-        self.max_temp_buttons = [tk.Button() for i in range(len(self.HEATER_CHANNELS))]
+        self.heater_buttons = [tk.Button() for i in range(len(HEATER_CHANNELS))]
+        self.max_temp_buttons = [tk.Button() for i in range(len(HEATER_CHANNELS))]
         
-        self.heater_buttons = [tk.Button() for i in range(len(self.HEATER_CHANNELS))]
-        for i in range(len(self.HEATER_CHANNELS)):
+        self.heater_buttons = [tk.Button() for i in range(len(HEATER_CHANNELS))]
+        for i in range(len(HEATER_CHANNELS)):
             
             self.duty[i].set(0)
-            row = tk.Frame(frame, bg=self.BG_COLOR, pady=10)
+            row = tk.Frame(frame, bg=BG_COLOR, pady=10)
             row.pack(fill=tk.X, padx=10, pady=5)
-            tk.Label(row, text=f"Heater {i+1}:", bg=self.BG_COLOR, font=self.FONT).pack(side=tk.LEFT, padx=5)
-            tk.Entry(row, width=4, font=self.FONT, textvariable=self.duty[i]).pack(side=tk.LEFT, padx=5)
+            tk.Label(row, text=f"Heater {i+1}:", bg=BG_COLOR, font=FONT).pack(side=tk.LEFT, padx=5)
+            tk.Entry(row, width=4, font=FONT, textvariable=self.duty[i]).pack(side=tk.LEFT, padx=5)
             
             # create Heater Buttons
             button = tk.Button(
-                row, text="Set", font=self.FONT, bg=self.OFF_COLOR, fg=self.BUTTON_TEXT_COLOR, relief=self.BUTTON_STYLE, width=4,
+                row, text="Set", font=FONT, bg=OFF_COLOR, fg=BUTTON_TEXT_COLOR, relief=BUTTON_STYLE, width=4,
                 command=lambda i=i: self.set_duty_value(i, self.duty[i])
             )
             button.pack(side=tk.LEFT, padx=5)
             self.heater_buttons[i] = button
             
             # create Max Temp Buttons
-            tk.Entry(row, width=4, font=self.FONT, textvariable=self.max_temp[i]).pack(side=tk.LEFT, padx=(20,5))
-            tk.Label(row, text ="°C", bg=self.BG_COLOR, font=self.FONT).pack(side=tk.LEFT,anchor=tk.W,pady=5)
+            tk.Entry(row, width=4, font=FONT, textvariable=self.max_temp[i]).pack(side=tk.LEFT, padx=(20,5))
+            tk.Label(row, text ="°C", bg=BG_COLOR, font=FONT).pack(side=tk.LEFT,anchor=tk.W,pady=5)
                 
             max_temp_button = tk.Button(
-                row, text="Max", font=self.FONT, bg=self.OFF_COLOR, fg=self.BUTTON_TEXT_COLOR, relief=self.BUTTON_STYLE,
+                row, text="Max", font=FONT, bg=OFF_COLOR, fg=BUTTON_TEXT_COLOR, relief=BUTTON_STYLE,
                 command=lambda i=i: self.set_max_temp(i, self.max_temp[i])
             )
             
@@ -62,44 +53,44 @@ class HeaterControlPanel:
 
             # create Autoset Button for Heater 1 only
             if i == 0:
-                self.autoset_frame = tk.Frame(row,bg=self.BG_COLOR,pady=10)
+                self.autoset_frame = tk.Frame(row,bg=BG_COLOR,pady=10)
                 self.autoset_frame.pack(side=tk.RIGHT,padx=5,pady=5)
                 autoset_temp=tk.StringVar()
-                tk.Entry(self.autoset_frame,width=3,font=self.FONT,textvariable=autoset_temp).pack(side=tk.LEFT,anchor=tk.NW,padx=(5,2),pady=5)
-                tk.Label(self.autoset_frame, text ="°C", bg=self.BG_COLOR, font=self.FONT).pack(side=tk.LEFT,anchor=tk.NW,pady=5)
-                self.autoset_button = tk.Button(self.autoset_frame,text="Autoset",font=self.FONT,bg=self.OFF_COLOR,fg=self.BUTTON_TEXT_COLOR,relief=self.BUTTON_STYLE,command=lambda:self.change_autoset(autoset_temp))
+                tk.Entry(self.autoset_frame,width=3,font=FONT,textvariable=autoset_temp).pack(side=tk.LEFT,anchor=tk.NW,padx=(5,2),pady=5)
+                tk.Label(self.autoset_frame, text ="°C", bg=BG_COLOR, font=FONT).pack(side=tk.LEFT,anchor=tk.NW,pady=5)
+                self.autoset_button = tk.Button(self.autoset_frame,text="Autoset",font=FONT,bg=OFF_COLOR,fg=BUTTON_TEXT_COLOR,relief=BUTTON_STYLE,command=lambda:self.change_autoset(autoset_temp))
                 self.autoset_button.pack(side=tk.LEFT,padx=5)
         
         # create MFC Flowrate button
-        mfc = tk.Frame(frame,bg=self.BG_COLOR,pady=10)
+        mfc = tk.Frame(frame,bg=BG_COLOR,pady=10)
         mfc.pack(anchor=tk.N,fill=tk.X,padx=10,pady=5)
         setpt=tk.StringVar()
-        tk.Label(mfc, text ="Setpoint: ", bg=self.BG_COLOR, font=self.FONT).pack(side=tk.LEFT,anchor=tk.NW,padx=5,pady=5)
-        tk.Entry(mfc,width=5,font=self.FONT,textvariable=setpt).pack(side=tk.LEFT,anchor=tk.NW,padx=5,pady=5)
-        self.setpoint_button = tk.Button(mfc,text="Change Setpoint",font=self.FONT,bg=self.OFF_COLOR,fg=self.BUTTON_TEXT_COLOR,relief=self.BUTTON_STYLE,command=lambda:self.change_setpt(setpt))
+        tk.Label(mfc, text ="Setpoint: ", bg=BG_COLOR, font=FONT).pack(side=tk.LEFT,anchor=tk.NW,padx=5,pady=5)
+        tk.Entry(mfc,width=5,font=FONT,textvariable=setpt).pack(side=tk.LEFT,anchor=tk.NW,padx=5,pady=5)
+        self.setpoint_button = tk.Button(mfc,text="Change Setpoint",font=FONT,bg=OFF_COLOR,fg=BUTTON_TEXT_COLOR,relief=BUTTON_STYLE,command=lambda:self.change_setpt(setpt))
         self.setpoint_button.pack(side=tk.LEFT,padx=5)
         
         ''' # reading from Alicat the true flowrate - disabled because it caused a lot of latency
-        self.flowrate_label = tk.Label(frame,text="Flowrate: ", bg=self.BG_COLOR, font=self.FONT)
+        self.flowrate_label = tk.Label(frame,text="Flowrate: ", bg=BG_COLOR, font=FONT)
         self.flowrate_label.pack(side=tk.TOP,anchor=tk.NW,padx=20,pady=5)
         self.update_setpoint_reading()
         '''   
         # heater key - (displays in reverse order)
-        tk.Label(frame, text=f"Heater 4 - Gauges", bg=self.BG_COLOR, font=self.FONT).pack(side=tk.BOTTOM, anchor=tk.SW, pady=5,padx=10)
-        tk.Label(frame, text=f"Heater 3 - Inlet, TMA", bg=self.BG_COLOR, font=self.FONT).pack(side=tk.BOTTOM, anchor=tk.SW, pady=5,padx=10)
-        tk.Label(frame, text=f"Heater 2 - Trap", bg=self.BG_COLOR, font=self.FONT).pack(side=tk.BOTTOM, anchor=tk.SW, pady=5,padx=10)
-        tk.Label(frame, text=f"Heater 1 - Chamber", bg=self.BG_COLOR, font=self.FONT).pack(side=tk.BOTTOM, anchor=tk.SW, pady=5,padx=10)
+        tk.Label(frame, text=f"Heater 4 - Gauges", bg=BG_COLOR, font=FONT).pack(side=tk.BOTTOM, anchor=tk.SW, pady=5,padx=10)
+        tk.Label(frame, text=f"Heater 3 - Inlet, TMA", bg=BG_COLOR, font=FONT).pack(side=tk.BOTTOM, anchor=tk.SW, pady=5,padx=10)
+        tk.Label(frame, text=f"Heater 2 - Trap", bg=BG_COLOR, font=FONT).pack(side=tk.BOTTOM, anchor=tk.SW, pady=5,padx=10)
+        tk.Label(frame, text=f"Heater 1 - Chamber", bg=BG_COLOR, font=FONT).pack(side=tk.BOTTOM, anchor=tk.SW, pady=5,padx=10)
         
         return frame
 
     # called when Set button is pressed for any heater, sends appropriate call to temp_controller including which thread to update
     def set_duty_value(self, i, duty_cycle_var):
         duty = float(duty_cycle_var.get())
-        if self.heater_buttons[i].cget("bg") == self.ON_COLOR or duty==0: # ON -> OFF
+        if self.heater_buttons[i].cget("bg") == ON_COLOR or duty==0: # ON -> OFF
             self.app.temp_controller.update_duty_cycle(self.app.temp_controller.queues[i],0)
-            self.heater_buttons[i].config(bg=self.OFF_COLOR) # turn button to off if it was on
+            self.heater_buttons[i].config(bg=OFF_COLOR) # turn button to off if it was on
         elif duty <= 100: # OFF -> ON
-            self.heater_buttons[i].config(bg=self.ON_COLOR) 
+            self.heater_buttons[i].config(bg=ON_COLOR) 
             self.app.temp_controller.update_duty_cycle(self.app.temp_controller.queues[i],duty)
             print(f"Heater {i+1} set to {duty}%")
         #except:
@@ -113,16 +104,16 @@ class HeaterControlPanel:
             max_temp = float(max_temp_var.get())
             if max_temp > 0:
                 self.app.log_controller.update_max_temp(i,max_temp)
-                self.max_temp_buttons[i].config(bg=self.ON_COLOR)
+                self.max_temp_buttons[i].config(bg=ON_COLOR)
             else:
                 self.app.log_controller.update_max_temp(i,300)
                 print(f"Max Temp Heater {i+1} set to Default 300")
-                self.max_temp_buttons[i].config(bg=self.OFF_COLOR)
+                self.max_temp_buttons[i].config(bg=OFF_COLOR)
         except:
             print(f"Invalid Input. Please enter a positive number") # turn into a console warning
             self.app.log_controller.update_max_temp(i,300)
             print(f"Max Temp Heater {i+1} set to Default 300")
-            self.max_temp_buttons[i].config(bg=self.OFF_COLOR)
+            self.max_temp_buttons[i].config(bg=OFF_COLOR)
     
     # Alicat change flowrate set point sccm
     def change_setpt(self,setpt_var):
@@ -130,35 +121,35 @@ class HeaterControlPanel:
             setpt = float(setpt_var.get())
             print(setpt)
             if setpt == 0:
-                self.setpoint_button.config(bg=self.OFF_COLOR)
+                self.setpoint_button.config(bg=OFF_COLOR)
             elif setpt > 0:
-                self.setpoint_button.config(bg=self.ON_COLOR)
+                self.setpoint_button.config(bg=ON_COLOR)
             else:
                 raise Exception()
             self.app.alicat.change_setpoint(setpoint_value=setpt)
         except:
             print("Invalid Setpoint")
-            self.setpoint_button.config(bg=self.OFF_COLOR)
+            self.setpoint_button.config(bg=OFF_COLOR)
     
     
     def change_autoset(self, autoset_temp_var):
         try:
             self.autoset_temp = float(autoset_temp_var.get())
             duty = self.duty[0]
-            if self.autoset_button["bg"] == self.ON_COLOR or self.autoset_temp == 0:
+            if self.autoset_button["bg"] == ON_COLOR or self.autoset_temp == 0:
                 print("Autoset Disabled")
-                self.autoset_button.config(bg=self.OFF_COLOR)
+                self.autoset_button.config(bg=OFF_COLOR)
                 self.heater_buttons[0].config(state=tk.NORMAL) # re-disable Set button for Heater 1
                 self.app.temp_controller.autoset.clear()
             elif self.autoset_temp > 0:
                 print("Autoset Enabled")
-                self.autoset_button.config(bg=self.ON_COLOR)
+                self.autoset_button.config(bg=ON_COLOR)
                 self.heater_buttons[0].config(state=tk.DISABLED) # disable Set button for Heater 1 when autoset is active
                 self.app.temp_controller.autoset.set()
                 self.app.temp_controller.autoset_queue.put(self.autoset_temp)
         except:
             print("Invalid input")
-            self.autoset_button.config(bg=self.OFF_COLOR)
+            self.autoset_button.config(bg=OFF_COLOR)
             self.heater_buttons[0].config(state=tk.NORMAL)
             self.app.temp_controller.autoset.clear()
             
--- gui_panels/main_power.py
+++ gui_panels/main_power.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from nidaqmx import Task
 from nidaqmx.constants import LineGrouping
+from config import *
 from controllers import log_controller
 
 ## Main Power
@@ -9,21 +10,13 @@ from controllers import log_controller
 class MainPower:
     def __init__(self, app):
         self.app = app
-        self.MAIN_POWER_CHANNEL = app.MAIN_POWER_CHANNEL
-        self.FONT = app.FONT
-        self.BUTTON_TEXT_COLOR = app.BUTTON_TEXT_COLOR
-        self.BUTTON_STYLE = app.BUTTON_STYLE
-        self.MONITOR_LOG_FILE = app.MONITOR_LOG_FILE
-        self.ON_COLOR = app.ON_COLOR
-        self.OFF_COLOR = app.OFF_COLOR
-
         self.task = self.create_main_power_task()
         self.app.log_controller.controllers_active_flag = False
 
     # creating nidaqmx task
     def create_main_power_task(self):
         task = Task("Main Power")
-        task.do_channels.add_do_chan(self.MAIN_POWER_CHANNEL, line_grouping=LineGrouping.CHAN_PER_LINE)
+        task.do_channels.add_do_chan(MAIN_POWER_CHANNEL, line_grouping=LineGrouping.CHAN_PER_LINE)
         task.start()
         task.write(False)
         task.stop()
@@ -32,7 +25,7 @@ class MainPower:
     # create button, default state OFF
     def create_main_power_button(self, parent):
         self.main_power_button = tk.Button(
-            parent, text='Main Power OFF', fg=self.BUTTON_TEXT_COLOR, bg=self.OFF_COLOR, relief=self.BUTTON_STYLE, font=self.FONT,
+            parent, text='Main Power OFF', fg=BUTTON_TEXT_COLOR, bg=OFF_COLOR, relief=BUTTON_STYLE, font=FONT,
             command=self.toggle_main_power
         )
         self.main_power_button.pack(pady=10)
@@ -48,16 +41,16 @@ class MainPower:
         self.task.start()
         self.task.write(False)
         self.task.stop()
-        self.main_power_button.config(text='Main Power OFF', bg=self.OFF_COLOR)
-        record = log_controller.create_record(f"MP, 0",self.MONITOR_LOG_FILE) # create a log entry when power is turned OFF
+        self.main_power_button.config(text='Main Power OFF', bg=OFF_COLOR)
+        record = log_controller.create_record(f"MP, 0",MONITOR_LOG_FILE) # create a log entry when power is turned OFF
         self.app.log_controller.monitor_queue.put(record)
         
     def main_power_on(self):
         self.task.start()
         self.task.write(True)
         self.task.stop()
-        self.main_power_button.config(text='Main Power ON', bg=self.ON_COLOR)
-        record = log_controller.create_record(f"MP, 1",self.MONITOR_LOG_FILE)  # create a log entry when power is turned ON
+        self.main_power_button.config(text='Main Power ON', bg=ON_COLOR)
+        record = log_controller.create_record(f"MP, 1",MONITOR_LOG_FILE)  # create a log entry when power is turned ON
         self.app.log_controller.monitor_queue.put(record)
         
         # flag that power has been turned on - needed to re-enable some functions after system overheat automatic shutoff
